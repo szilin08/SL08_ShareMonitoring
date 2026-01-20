631015dc15fdd0ec5bdd9ad16da14a490b528777
szilin08 <153799833+szilin08@users.noreply.github.com>
2026-01-20 11:31:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
 import streamlit as st
 st.set_page_config(page_title="LBS Bina Competitor Dashboard", layout="wide")
+st.cache_data.clear()
 
 import stock_monitoring, financials, balance_sheet, cash_flow, overview, dividend, esg
 
@@ -20,6 +21,7 @@ with tabs[5]:
     dividend.main()
 with tabs[6]:
     esg.main()
+
 
 
 
